0bb2227f845eb01c7b265f902d7b996d16d9ddef
Satyam kumar <satyamkumar2716@gmail.com>
2026-04-17 03:02:55 +0530

    change

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const allowedOrigins = [
-  'https://printer-setup1.vercel.app/',
   'https://printer-setup1.vercel.app',
   'http://localhost:5173',
   'http://localhost:3000',
